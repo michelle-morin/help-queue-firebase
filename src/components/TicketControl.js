eb88a21ed73def50d89059b7da0d4c653b008da9
Michelle Morin <morin2m@gmail.com>
2020-04-20 15:43:33 -0700

    add functionality to switch to form view when user clicks add ticket button

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -9,16 +9,24 @@ class TicketControl extends React.Component {
       formVisibleOnPage: false
     };
   }
+
+  handleClick = () => {
+    this.setState({formVisibleOnPage: true});
+  }
+
   render(){
     let currentlyVisibleState = null;
+    let addTicketButton = null;
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm />
     } else {
       currentlyVisibleState = <TicketList />
+      addTicketButton = <button onClick={this.handleClick}>Add ticket</button>
     }
     return (
       <React.Fragment>
         {currentlyVisibleState}
+        {addTicketButton}
       </React.Fragment>
     );
   }
