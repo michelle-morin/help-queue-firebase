bc07145f3799e8eca3b8abbf2f2a1407a17b7df6
Michelle Morin <morin2m@gmail.com>
2020-04-20 15:48:54 -0700

    add button to toggle between views

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -11,22 +11,25 @@ class TicketControl extends React.Component {
   }
 
   handleClick = () => {
-    this.setState({formVisibleOnPage: true});
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
   }
 
   render(){
     let currentlyVisibleState = null;
-    let addTicketButton = null;
+    let buttonText = null;
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm />
+      buttonText = "Return to Ticket List"
     } else {
       currentlyVisibleState = <TicketList />
-      addTicketButton = <button onClick={this.handleClick}>Add ticket</button>
+      buttonText = "Add Ticket";
     }
     return (
       <React.Fragment>
         {currentlyVisibleState}
-        {addTicketButton}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
