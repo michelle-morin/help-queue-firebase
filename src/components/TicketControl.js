ac2a46b7a56ecd0a053e31f9759bc5f8f65e5177
Michelle Morin <morin2m@gmail.com>
2020-04-21 18:35:47 -0700

    create method to handle form submission, pass method down to child component as prop, and utilize parent component's method in child component

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -17,6 +17,11 @@ class TicketControl extends React.Component {
     }));
   }
 
+  handleAddingNewTicketToList = (newTicket) => {
+    const newMasterTicketList = this.state.masterTicketList.concat(newTicket);
+    this.setState({masterTicketList: newMasterTicketList, formVisibleOnPage: false});
+  }
+
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
