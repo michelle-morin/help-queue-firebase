076c3bb18d9a2c81107e717b7dd2d01c03191f9a
Michelle Morin <morin2m@gmail.com>
2020-04-21 18:45:34 -0700

    add inline css styles to new ticket form component

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -9,22 +9,37 @@ function NewTicketForm(props){
     props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: v4()});
   }
 
+  const newTicketFormStyles = {
+    backgroundColor: '#f5f5f5',
+    outline: 'none',
+    boxShadow: 'none',
+    padding: '5%',
+    margin: 'auto auto',
+    border: '1px solid #111111',
+    borderRadius: '10px'
+  }
+
   return (
     <React.Fragment>
-      <form onSubmit={handleNewTicketFormSubmission}>
-        <input
-          type='text'
-          name='names'
-          placeholder='Pair Names' />
-        <input
-          type='text'
-          name='location'
-          placeholder='Location' />
-        <textarea
-          name='issue'
-          placeholder='Describe your issue.' />
-        <button type='submit'>Help!</button>
-      </form>
+      <div style={newTicketFormStyles}>
+        <form onSubmit={handleNewTicketFormSubmission}>
+          <input
+            type='text'
+            name='names'
+            placeholder='Pair Names' />
+          <br/>
+          <input
+            type='text'
+            name='location'
+            placeholder='Location' />
+          <br/>
+          <textarea
+            name='issue'
+            placeholder='Describe your issue.' />
+          <br/>
+          <button type='submit'>Help!</button>
+        </form>
+      </div>
     </React.Fragment>
   );
 }
