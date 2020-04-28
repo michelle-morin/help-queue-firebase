a4b87c69d1618bd8dbd8de83828b6ff8806cfcab
Michelle Morin <morin2m@gmail.com>
2020-04-27 17:48:30 -0700

    refactor to handle form visibility state with redux

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -10,7 +10,6 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false,
       selectedTicket: null,
       editing: false
     };
@@ -19,14 +18,15 @@ class TicketControl extends React.Component {
   handleClick = () => {
     if (this.state.selectedTicket != null) {
       this.setState({
-        formVisibleOnPage: false,
         selectedTicket: null,
         editing: false
       });
     } else {
-      this.setState(prevState => ({
-        formVisibleOnPage: !prevState.formVisibleOnPage,
-      }));
+      const { dispatch } = this.props;
+      const action = {
+        type: 'TOGGLE_FORM'
+      }
+      dispatch(action);
     }
   }
 
@@ -41,7 +41,10 @@ class TicketControl extends React.Component {
       issue: issue
     }
     dispatch(action);
-    this.setState({formVisibleOnPage: false});
+    const actionTwo = {
+      type: 'TOGGLE_FORM'
+    }
+    dispatch(actionTwo);
   }
 
   handleChangingSelectedTicket = (id) => {
@@ -95,7 +98,7 @@ class TicketControl extends React.Component {
         onClickingDelete = {this.handleDeletingTicket} 
         onClickingEdit = {this.handleEditClick} />
       buttonText = "Return to Ticket List";
-    } else if (this.state.formVisibleOnPage) {
+    } else if (this.props.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
       buttonText = "Return to Ticket List";
     } else {
