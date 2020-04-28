c025f8977523c05da155c8e2f6257bafd5f2656b
Michelle Morin <morin2m@gmail.com>
2020-04-27 17:45:07 -0700

    import new root reducer into application entry point file and pass root reducer into store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './components/App.js';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
-import reducer from './reducers/ticket-list-reducer';
+import rootReducer from './reducers/index';
 import { Provider } from 'react-redux';
 
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
