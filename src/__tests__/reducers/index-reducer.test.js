2da105861045aeb44728483af12dc4f00591556c
Michelle Morin <morin2m@gmail.com>
2020-04-28 18:14:06 -0700

    refactor to use action constants

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -2,6 +2,7 @@ import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 import ticketListReducer from '../../reducers/ticket-list-reducer';
+import * as c from './../../actions/ActionTypes';
 
 let store = createStore(rootReducer);
 
@@ -24,20 +25,20 @@ describe("rootReducer", () => {
 
   test('Check that initial state of ticketListReducer matches root reducer', () => {
     const action = {
-      type: 'ADD_TICKET',
+      type: c.ADD_TICKET,
       names: 'Ryan & Aimen',
       location: '4b',
       issue: 'Redux action is not working correctly.',
       id: 1
-    }
+    };
     store.dispatch(action);
     expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, action));
   });
   
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     const action = {
-      type: 'TOGGLE_FORM'
-    }
+      type: c.TOGGLE_FORM
+    };
     store.dispatch(action);
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
   });
