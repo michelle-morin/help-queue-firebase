81dec466f05b81ba5b4ce755c003b8c0fdb3b4a9
Michelle Morin <morin2m@gmail.com>
2020-04-29 18:53:23 -0700

    add wait time to the help queue

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -1,5 +1,6 @@
 import ticketListReducer from '../../reducers/ticket-list-reducer';
 import * as c from './../../actions/ActionTypes';
+import Moment from 'moment';
 
 describe('ticketListReducer', ()=> {
 
@@ -9,6 +10,7 @@ describe('ticketListReducer', ()=> {
     names: "Ryan & Aimen",
     location: '4b',
     issue: 'Redux action is not working correctly',
+    timeOpen : 0,
     id: 1
   };
 
@@ -28,20 +30,24 @@ describe('ticketListReducer', ()=> {
   });
 
   test('should successfully add new ticket data to masterTicketList', ()=> {
-    const { names, location, issue, id } = ticketData;
+    const { names, location, issue, timeOpen, id } = ticketData;
     action = {
       type: c.ADD_TICKET,
       names: names,
       location: location,
       issue: issue,
-      id: id
+      timeOpen: timeOpen,
+      id: id,
+      formattedWaitTime: new Moment().fromNow(true)
     }
     expect(ticketListReducer({}, action)).toEqual({
       [id] : {
         names: names,
         location: location,
         issue: issue,
-        id: id
+        timeOpen: timeOpen,
+        id: id,
+        formattedWaitTime: 'a few seconds'
       }
     });
   });
@@ -59,4 +65,22 @@ describe('ticketListReducer', ()=> {
     });
   });
 
+  test('Should add a formatted wait time to ticket entry', () => {
+    const { names, location, issue, timeOpen, id } = ticketData;
+    action = {
+      type: c.UPDATE_TIME,
+      formattedWaitTime: '4 minutes',
+      id: id
+    };
+    expect(ticketListReducer({ [id] : ticketData }, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id,
+        formattedWaitTime: '4 minutes'
+      }
+    });
+  });
 });
