ebc51f3fd122e35e62c46b3c7a3a02dd06349e75
Michelle Morin <morin2m@gmail.com>
2020-05-04 21:15:50 -0700

    add nav links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header(){
   const headerStyles = {
     textAlign: 'center',
     color: '#008081',
-    fontSize: '3rem'
   }
   return (
     <div style={headerStyles}>
       <h1>Help Queue</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/signin">Sign In</Link>
+        </li>
+      </ul>
     </div>
   );
 }
